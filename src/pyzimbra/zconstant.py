58df567eaadab302b2e3f7a3baf908167e8b1dbc
Ilgar Mashayev <ilgar@mashayev.com>
2010-06-24 14:59:51 +0200

    added license headers

--- src/pyzimbra/zconstant.py
+++ src/pyzimbra/zconstant.py
@@ -1,7 +1,30 @@
 # -*- coding: utf-8 -*-
 """
+################################################################################
+# Copyright (c) 2010, Ilgar Mashayev
+# 
+# E-mail: pyzimbra@lab.az
+# Website: http://github.com/ilgarm/pyzimbra
+################################################################################
+# This file is part of pyzimbra.
+# 
+# Pyzimbra is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Pyzimbra is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pyzimbra.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
 @author: ilgar
 """
+
 
 SOAP_URL = '/service/soap'
 
